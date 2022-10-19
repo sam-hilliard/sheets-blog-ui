2d4b50eb0139309e336a7d9f81750580218b8bdd
Sam Hilliard <samhilliard51@gmail.com>
2022-10-18 23:36:53 -0700

    rendered posts using papaparse

--- client/src/components/Pages/PostListPage.js
+++ client/src/components/Pages/PostListPage.js
@@ -1,15 +1,33 @@
 import React from 'react'
+import { useEffect, useState } from 'react'
 import { Typography } from '@mui/material'
 import BlogPost from '../BlogPost';
+import { getBlogPosts } from '../../hooks/utils/getBlogPosts';
 
 export default function PostListPage() {
-  return (
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        getBlogPosts().then(res => setPosts([...res]))
+      }, [])    
+
+    return (
     <>
         <Typography variant="h2" component="h2">Blog Posts</Typography>
         <div className="bloglist-container">
-            <BlogPost title="some title" description="some description" />
+            {posts.map(post => {
+                return(
+                    <BlogPost
+                        key={post.slug}
+                        title={post.title}
+                        slug={post.slug}
+                        pubdate={post.pupdate}
+                    />
+                )
+            })}
         </div>
     </>
-    
-  )
+
+    )
 }
